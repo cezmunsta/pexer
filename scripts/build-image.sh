36b8e20022c64403a03c50c785fb4a313bb6563f
Ceri Williams <github@incoming-email.co.uk>
2024-06-04 12:15:16 +0100

    Updated el9 to use UBI 9 (#3)
    * Replaced CentOS Stream 9 with UBI 9 for `all_el9`
    * Added `all_ol9` to build for Oracle Linux 9
    * Added `all_stream9` to build for CentOS Stream 9

--- scripts/build-image.sh
+++ scripts/build-image.sh
@@ -24,7 +24,7 @@ function setup {
     case "${DISTRO}" in
         # TODO: add support for UBI - ubi, ubi-minimal, ubi-init
         ## https://developers.redhat.com/products/rhel/ubi
-        "centos:stream8"|"centos:stream9"|"ubi8"|"oraclelinux:8") setup_redhat "${@}";;
+        "centos:stream8"|"centos:stream9"|"ubi8"|"ubi9"|"oraclelinux:8"|"oraclelinux:9") setup_redhat "${@}";;
         # TODO: remove el7 support completely, leaving as disabled now in case it is required
         #"centos:7") setup_redhat_legacy "${@}";;
         "ubuntu:22.04"|"ubuntu:jammy"|"debian:bullseye"|"debian:11") setup_debian "${@}";;
@@ -69,7 +69,9 @@ function setup_redhat {
 
     case "${DISTRO}" in
         "centos:stream8"|"ubi8"|"oraclelinux:8") packages+=( python38 python38-wheel python39 python39-wheel );;
-        "centos:stream9") packages+=( python3 python-wheel-wheel ); repos=( "--enablerepo=crb" );
+        "ubi9") packages+=( python3 python-wheel-wheel );;
+        "oraclelinux:9") packages+=( python3 python3-wheel-wheel ); repos=( "--enablerepo=ol9_codeready_builder" );;
+        "centos:stream9") packages+=( python3 python-wheel-wheel ); repos=( "--enablerepo=crb" );;
     esac
 
     dnf install -y "${repos[@]}" "${packages[@]}"
